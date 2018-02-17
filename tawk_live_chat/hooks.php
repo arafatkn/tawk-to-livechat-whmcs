75eb5e38b94ce4380c225b0fbd6be2ef68650d89
Arafat Islam <arafatkn@users.noreply.github.com>
2018-02-17 21:05:44 +0600

    Update hooks.php

--- tawk_live_chat/hooks.php
+++ tawk_live_chat/hooks.php
@@ -4,15 +4,16 @@ if (!defined("WHMCS"))
 	die("This file cannot be accessed directly");
 
 function tawk_live_chat_ClientAreaFooterOutput($vars) {
-	$sql = "SELECT value FROM `tbladdonmodules` WHERE setting = 'clientareacode' AND module = 'tawk_live_chat';";
-	$sql = mysqli_query($sql);
-	if(mysqli_num_rows($sql) > 0)
-	{
-		$data = mysqli_fetch_assoc($sql);
-		$value = html_entity_decode($data["value"]);
-	} else {
-		$value = '';
-	}
+	
+    $value = "";
+    try {	
+    	foreach(Capsule::table('tbladdonmodules')->where('setting', 'clientareacode')->where('module', 'tawk_live_chat')->get() as $data)
+    	{
+    	    $value = html_entity_decode($data->value);
+    	}
+    }  catch (Exception $e) {
+        echo "Something is wrong. {$e->getMessage()}";
+    }
     return "{$value}";
 }
 
